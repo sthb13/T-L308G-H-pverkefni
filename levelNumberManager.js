aebb596862b87e551bfb7bb92abe5ca7ac3f1df2
birkirfreyrg <birkirfrey@msn.com>
2021-11-10 20:02:12 +0000

    Added methods to score, life and Level Number
    Added Methods so everything should be ready when collision is done.

--- levelNumberManager.js
+++ levelNumberManager.js
@@ -7,7 +7,7 @@ var levelNumberManager = {
 levelArray : [],
 levelText : [],
 levelNumber : 1,
-x : 0,
+xOffset : 4,
 y : 748,
 levelTextWidth : GRID_BLOCK_W*25,
 //height : 44,
@@ -17,7 +17,7 @@ update: function(du) {
 },
 
 initialize: function() {
-    // Initialize the Scoreboard
+    // Initialize the Level number
     //drawLevelTxt();
 },
 
@@ -26,24 +26,24 @@ drawLevelTxt: function() {
   g_ctx.font = '31px lode_runner_c64regular';
   g_ctx.fillStyle = "rgba(13,161,255,255)";
   var ctext = "LEVEL".split("").join(String.fromCharCode(8202));
-  g_ctx.fillText(ctext, this.levelTextWidth-(GRID_BLOCK_W*5), this.y);
+  g_ctx.fillText(ctext, this.levelTextWidth-(GRID_BLOCK_W*5)+this.xOffset, this.y);
 },
 
   // Draw the level number
 drawLevelNumber: function() {
   g_ctx.font = '31px lode_runner_c64regular';
   g_ctx.fillStyle = "rgba(242,94,0,255)";
-  var ctext = (""+this.leadingZeros(this.levelNumber)).split("").join(String.fromCharCode(8202));
-	g_ctx.fillText(ctext, this.levelTextWidth, this.y);
+  var ctext = (""+this.leadingZeros(3, this.levelNumber)).split("").join(String.fromCharCode(8202));
+	g_ctx.fillText(ctext, this.levelTextWidth+this.xOffset, this.y);
 },
 
+// When player wins a level and continues to the next, increment level number.
 nextLevel: function() {
   this.levelNumber++;
 },
 
-
-leadingZeros: function(num) {
-  var size = 3;
+// get leading zeros like in the original game.
+leadingZeros: function(size, num) {
   var s = num+"";
   while(s.length < size) s = "0" + s;
   return s;
--- lifeManager.js
+++ lifeManager.js
@@ -7,7 +7,7 @@ var lifeManager = {
 lifeArray : [],
 lifeText : [],
 lifeNumber : 5,
-x : 0,
+xOffset : 4,
 y : 748,
 lifeWidth : GRID_BLOCK_W*16,
 //height : 44,
@@ -17,7 +17,7 @@ update: function(du) {
 },
 
 initialize: function() {
-    // Initialize the Scoreboard
+    // Initialize the life
     //drawLifeTxt();
 },
 
@@ -26,27 +26,20 @@ drawLifeTxt: function() {
   g_ctx.font = '31px lode_runner_c64regular';
   g_ctx.fillStyle = "rgba(13,161,255,255)";
   var ctext = "MEN".split("").join(String.fromCharCode(8202));
-  g_ctx.fillText(ctext, this.lifeWidth-(GRID_BLOCK_W*3), this.y);
+  g_ctx.fillText(ctext, this.lifeWidth-(GRID_BLOCK_W*3)+this.xOffset, this.y);
 },
 
   // Draw the life number
 drawLifeNumber: function() {
   g_ctx.font = '31px lode_runner_c64regular';
   g_ctx.fillStyle = "rgba(242,94,0,255)";
-  var ctext = (""+this.leadingZeros(this.lifeNumber)).split("").join(String.fromCharCode(8202));
-	g_ctx.fillText(ctext, this.lifeWidth, this.y);
+  var ctext = (""+levelNumberManager.leadingZeros(3, this.lifeNumber)).split("").join(String.fromCharCode(8202));
+	g_ctx.fillText(ctext, this.lifeWidth+this.xOffset, this.y);
 },
 
+// When player dies he looses 1 life.
 looseLife: function() {
   this.lifeNumber--;
-},
-
-
-leadingZeros: function(num) {
-  var size = 3;
-  var s = num+"";
-  while(s.length < size) s = "0" + s;
-  return s;
 },
 
 render: function(ctx) {
